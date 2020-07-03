9589ceb237e120d271adf6399c5037aa3a76ffc2
Nicholas Azzarello <nicko789@Melissas-MacBook-Pro.local>
2020-07-03 17:10:06 -0400

    Updates

---
+++ precache-manifest.517d74f4b8f3bc7be6fbe2a69170ff5b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c9f1956f5aca5a1799d941a8c0b0db",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "20db1dc98f9c219ab4c0",
+    "url": "/portfolio/static/css/main.1f416336.chunk.css"
+  },
+  {
+    "revision": "e7169b9aaae2f9df9a12",
+    "url": "/portfolio/static/js/2.0b4f052f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0b4f052f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20db1dc98f9c219ab4c0",
+    "url": "/portfolio/static/js/main.0a3a01cc.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "390795142a5b608c3966420f8805ec83",
+    "url": "/portfolio/static/media/agape.39079514.png"
+  },
+  {
+    "revision": "1aab3af814b45ac3dda20e362d4bdf99",
+    "url": "/portfolio/static/media/angstadt-family-dental.1aab3af8.png"
+  },
+  {
+    "revision": "a0910148246ab1c8f1be54618afa03be",
+    "url": "/portfolio/static/media/bb-integrations.a0910148.png"
+  },
+  {
+    "revision": "4f47a01e9a30d6aeed82a007da6b3eec",
+    "url": "/portfolio/static/media/bg-glass.4f47a01e.png"
+  },
+  {
+    "revision": "73c900dcb21626a4f0c775aa86d7eb3f",
+    "url": "/portfolio/static/media/catholic-prayers-skill.73c900dc.png"
+  },
+  {
+    "revision": "71be6addc89cd9789f561c7af60d033f",
+    "url": "/portfolio/static/media/christmas-guitar-skill.71be6add.png"
+  },
+  {
+    "revision": "b573fdadcbf6d0b72bdbface2398d7cd",
+    "url": "/portfolio/static/media/cleanup-skill.b573fdad.png"
+  },
+  {
+    "revision": "a3e9f0b9115d0242debef783f31a60e0",
+    "url": "/portfolio/static/media/cow-sounds-skill.a3e9f0b9.png"
+  },
+  {
+    "revision": "ddeb5b57cd43e0ca7a04fd905a9133fc",
+    "url": "/portfolio/static/media/eagles-chant-skill.ddeb5b57.png"
+  },
+  {
+    "revision": "92ec9a0c6c1436b42827248d4a46e2be",
+    "url": "/portfolio/static/media/elephant-sounds-skill.92ec9a0c.png"
+  },
+  {
+    "revision": "b436aab3dbfdc647018585c2eb1dbc39",
+    "url": "/portfolio/static/media/hamlet-quotes-skill.b436aab3.png"
+  },
+  {
+    "revision": "8ea03d9d561d1a61ebc06bd5a390a65d",
+    "url": "/portfolio/static/media/hero-image.8ea03d9d.jpg"
+  },
+  {
+    "revision": "f80aa909fe59c8747483d864f7587baa",
+    "url": "/portfolio/static/media/hoffmann-publishing.f80aa909.png"
+  },
+  {
+    "revision": "f2e38442279f1f613c770255cd94b23e",
+    "url": "/portfolio/static/media/italy-sights-skill.f2e38442.png"
+  },
+  {
+    "revision": "ec2ad6d03b65b3198f899bd9d661dc3c",
+    "url": "/portfolio/static/media/most-famous-princess-bride-quote.ec2ad6d0.png"
+  },
+  {
+    "revision": "14b121c4958a0d4b919259c3f85141c1",
+    "url": "/portfolio/static/media/phillies-camera-opereator.14b121c4.jpg"
+  },
+  {
+    "revision": "23273fd3336d2a2e545e287cb473dc93",
+    "url": "/portfolio/static/media/placeholder.23273fd3.jpg"
+  },
+  {
+    "revision": "73d9abaecf771cd58669bd3e40e3e357",
+    "url": "/portfolio/static/media/ring-doorbell-skill.73d9abae.png"
+  },
+  {
+    "revision": "da3840488062e76a6663c1f1e0eabec6",
+    "url": "/portfolio/static/media/romeo-and-juliet-quotes-skill.da384048.png"
+  },
+  {
+    "revision": "9507e18578a6f70d34057c19693175fe",
+    "url": "/portfolio/static/media/royals-camera-operator.9507e185.jpg"
+  },
+  {
+    "revision": "4e39832528cfc6ea39e0e90ec819ae95",
+    "url": "/portfolio/static/media/rubright.4e398325.png"
+  },
+  {
+    "revision": "f0cf332c8a04b396fa137f894b993c8f",
+    "url": "/portfolio/static/media/the-ten-commandments-skill.f0cf332c.png"
+  },
+  {
+    "revision": "043c1f14adb0ca45cd2899260f63c960",
+    "url": "/portfolio/static/media/the-ugly-barnacle-skill.043c1f14.png"
+  },
+  {
+    "revision": "ccaf081873bb4c5a3a4cca3d1d9aa598",
+    "url": "/portfolio/static/media/titanic-skill.ccaf0818.png"
+  },
+  {
+    "revision": "449079fc41fc646f68ef9071dddf07fc",
+    "url": "/portfolio/static/media/wake-up-skill.449079fc.png"
+  }
+]);
