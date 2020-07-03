fcb28533e300f8f05c965b6c31d7a63e0d4d69c3
Nicholas Azzarello <nicko789@Melissas-MacBook-Pro.local>
2020-07-03 17:21:27 -0400

    Updated contact page

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -14,7 +14,7 @@ const ContactMe = () => {
             </Helmet>
 
             <h1 className="center title">Contact Me</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+            <p>Contact Me Content! Lorem ipsum dolor sit amet, consectetur adipiscing 
             elit, sed do eiusmod tempor incididunt ut labore et dolore 
             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
             ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
